869baa8a6c897ad032ac4809e4fbbd319dcbfe6e
Mura75 <zmurager@gmail.com>
2017-03-27 20:32:02 +0600

    Google map sample project.

---
+++ googlemapproject/src/main/java/example/com/googlemapproject/MainActivity.java
@@ -0,0 +1,39 @@
+package example.com.googlemapproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap googleMap;
+
+    private SupportMapFragment mapFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.mapFragment);
+
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+
+        this.googleMap.setMyLocationEnabled(true);
+
+        this.googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+        this.googleMap.getUiSettings().setCompassEnabled(true);
+    }
+}
